2abf7da16334d4437bbc6edb9dbd5469d9eb6e56
Mark S. Brown <mbrown@ee.ucl.ac.uk>
2014-02-06 15:15:37 +0000

    updated dipoleexample

--- dipoleexample/dipoleexample.py
+++ dipoleexample/dipoleexample.py
@@ -1,71 +1,36 @@
-from __future__ import division,print_function
+####
+# DipoleArray Parameter Sweep Example
+####
 
+from __future__ import division,print_function
 import os
+import pylumerical as pyl
+from numpy import linspace
 
 ##consts
 nm = 1e-9
 
-##PyLumerical Setup
-import pylumerical as pyl
 workingdir = '/tmp/lumerical'
-datadir = os.getcwd()
-rootloc = pyl.full('originalscripts',datadir,verbose=1) #Lumerical geometry scripts to play with
-scriptloc = pyl.full('processingscripts',datadir,verbose=1) #Lumerical scripts to act on processed data
-originalscript = (scriptloc,'farfieldsave')
-tmpscript = (scriptloc,'Tmp')
+scriptloc = os.path.join(os.getcwd(),'originalscripts')
+processingloc = os.path.join(os.getcwd(),'processingscripts')
 
-##setting up
-lsfloc = pyl.full('input',workingdir,verbose=1) #input
-fsploc = pyl.full('fsp',workingdir,verbose=1) #inbetween
-dataloc = pyl.full('output',workingdir,verbose=1) #output
-
-defaultparams = {'BravaisTheta': 90, 'LX': 100*nm, 'LY': 100*nm, 'MonitorMargin': 50*nm,
-                'MarginXY' : 50*nm, 'MarginZ' : 50*nm, 'N01': 0, 'N02': 0, 'N11': 0, 
+defaultparams = {'BravaisTheta': 90, 'LX': 100*nm, 'LY': 100*nm, 'MonitorMargin': 2*nm,
+                'MarginXY' : 200*nm, 'MarginZ' : 100*nm, 'N01': 0, 'N02': 0, 'N11': 0, 
                 'N12': 0, 'MonitorLoc': 0, 'phi': 0,'theta': 0}
 
-customname = lambda params : "_".join(['DipoleArray']+params)
-MonitorLocation = {0:'x',2:'y',4:'z'}
-lsffiles = []
-#/setting up
+newparams = [('MonitorLoc' , [2]),
+             ('MarginXY' , linspace(100,200,4)*nm)]
 
-for N in range(0,11,2): #parameter(s) we're sweeping through
-    for aloc in MonitorLocation:
-        OverrideThese = {"MonitorLoc":aloc,"N12":N,"N11":N} #NxN array
-        newparams = dict(defaultparams.items()+OverrideThese.items())
-        
-        lsfname = customname([MonitorLocation[aloc],str(N)])
-        lsffiles.append(lsfname)
-        print("--",lsfname,"--")
-        
-        root = (rootloc,'DipoleArray')
-        lsf = (lsfloc,lsfname)
-        fsp = (fsploc,lsfname)
-        pyl.GenerateLSFinput(root,lsf,fsp,newparams)
+fsploc, outputloc = pyl.ParameterSweepInput(workingdir, 'MarginVary', newparams,
+                     defaultparams, (scriptloc, 'DipoleArray'), verbose=0)
 
-print("Input :: lsf Scripts Generated")        
+print("Input lsf & FSP Generated")
 
-for anlsf in lsffiles:
-    pyl.GenerateFSPinput((lsfloc,anlsf),verbose=0)
-    
-print("FSP :: fsp binary files Generated")
-Success = pyl.ExecuteFSPfiles(fsploc,verbose=0)
+pyl.ExecuteFSPfiles(fsploc, cores=8, verbose=0)
+print("FSP Processed")
 
-print("FSP :: all fsp files completed with signal",Success)
-
-for fspname in os.listdir(fsploc):
-    if not fspname.endswith("fsp"):
-        continue
-    
-    ##Generate new script (new save location per fsp file)
-    fullpath = os.path.join(fsploc,fspname.split('.')[0])
-    variables = {'Monitor' : "PowerMonitor", 'Savefullpath' : os.path.join(dataloc,fspname)}
-    pyl.AlterVariables(originalscript,tmpscript,variables,verbose=0)
-    
-    fsp = (fsploc,fspname)
-    Success = pyl.ExecuteScriptOnFSP(fsp,tmpscript,verbose=0)
-    
-    print(fspname,"returns with code",Success)
-    
-
-print("Output csv files generated")
+scriptparams = {'Monitor' : "PowerMonitor"}
+pyl.ProcessGenerated(fsploc, outputloc, processingloc, 'farfieldsave', scriptparams, verbose=1)
 print("Complete!")
+
+
